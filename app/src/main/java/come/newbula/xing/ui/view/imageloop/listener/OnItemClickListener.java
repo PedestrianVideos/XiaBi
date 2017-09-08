f7b50aad0f79bcb6c5efb0c65a14a44d7673c3d5
beansDou <itnh61808@163.com>
2017-09-08 15:16:02 +0800

    添加轮播图

---
+++ app/src/main/java/come/newbula/xing/ui/view/imageloop/listener/OnItemClickListener.java
@@ -0,0 +1,8 @@
+package come.newbula.xing.ui.view.imageloop.listener;
+
+/**
+ * Created by Sai on 15/11/13.
+ */
+public interface OnItemClickListener {
+    public void onItemClick(int position);
+}
