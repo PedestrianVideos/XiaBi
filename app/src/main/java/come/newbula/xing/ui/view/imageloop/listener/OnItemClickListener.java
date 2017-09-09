be270c2b8df7aacef3050f92dc3956d80bd42cf6
AndroidMa <18861852514@163.com>
2017-09-09 09:50:14 +0800

    s

--- app/src/main/java/come/newbula/xing/ui/view/imageloop/listener/OnItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package come.newbula.xing.ui.view.imageloop.listener;
-
-/**
- * Created by Sai on 15/11/13.
- */
-public interface OnItemClickListener {
-
-    public void onItemClick(int position);
-}
